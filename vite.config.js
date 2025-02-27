6f0f305741261b3a5b768e0cc0bfc9f2b79aeb87
S. Choukah <21149759+schoukah@users.noreply.github.com>
2025-02-26 22:24:54 -0500

    changed base folder

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 // import path from 'path'
 
 export default defineConfig({
-    // base: 'https://flaneries.net/',
+    base: ' https://schoukah.github.io/',
     build: {
     outDir : 'docs',
     rollupOptions: {
